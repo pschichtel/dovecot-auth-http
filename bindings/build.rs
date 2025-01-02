fb1dec67c8def03b6017652b06b0093a9d53522b
Phillip Schichtel <phillip@schich.tel>
2025-01-02 20:21:32 +0100

    dovecot is messy to extend, let's stop this here

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,9 +1,21 @@
-use std::env;
+use std::{env, fs};
 use std::path::PathBuf;
 
 fn main() {
+    let dovecot_dir = fs::canonicalize(env::current_dir().unwrap().join("dovecot")).unwrap();
+    let src_dir = dovecot_dir.join("src");
+    let lib_dir = src_dir.join("lib");
+    let lib_storage_dir = src_dir.join("lib-storage");
+    let lib_mail_dir = src_dir.join("lib-mail");
+    let lib_index_dir = src_dir.join("lib-index");
     let bindings = bindgen::Builder::default()
-        .headers(["dovecot/..."])
+        .clang_arg(format!("--include-directory={}", dovecot_dir.display()))
+        .clang_arg(format!("--include-directory={}", src_dir.display()))
+        .clang_arg(format!("--include-directory={}", lib_storage_dir.display()))
+        .clang_arg(format!("--include-directory={}", lib_mail_dir.display()))
+        .clang_arg(format!("--include-directory={}", lib_index_dir.display()))
+        .clang_arg(format!("--include-directory={}", lib_dir.display()))
+        .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
---
+++ bindings/wrapper.h
@@ -0,0 +1,9 @@
+
+// #include <stdbool.h>
+// #include <coccinelle/macros.h>
+// #include <lib/macros.h>
+// #include <lib-auth/auth-client.h>
+// #include <lib-auth/auth-client-interface.h>
+
+#include <lib-storage/mail-storage-hooks.h>
+#include <imap/imap-common.h>
